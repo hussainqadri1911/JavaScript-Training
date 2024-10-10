2034532a62598cff2ff460fae84be49240c81031
hussainqadri1911 <yourownwebdeveloper@gmail.com>
2024-10-10 02:48:27 +0000

    Conversion of DataTypes

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,50 @@
-console.log("Muhammad Hussain")
+// console.log("Muhammad Hussain")
+
+// let score = "22Hussain"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valInNum = Number(score)
+// console.log(typeof valInNum)
+// console.log(valInNum)
+
+// let valu = null
+// console.log(typeof valu)
+
+// let valuInNum = Number(valu)
+// console.log(typeof valuInNum)
+// console.log(valuInNum)
+
+// By applying Number(left), values converted into right.
+// left                 right
+// 22                   => 22
+// "Muhammad Hussain"   => NaN
+// "22Hussain"          => NaN
+// undefined            => NaN
+// null                 => 0
+// false                => 0
+// true                 => 1
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let notLoggedIn = 0
+// let booleanNotLoggedIn = Boolean(notLoggedIn)
+// console.log(booleanNotLoggedIn)
+
+// By applying Boolean(left), values converted into right.
+// left                 right
+// 0                 => false
+// 1                 => true
+// ""                => false
+// "Hussain"         => true
+
+let anyNum = false
+let strAnyNum = String(anyNum)
+console.log(strAnyNum)
+console.log(typeof strAnyNum)
+
+// By applying String(left), values converted into right.
+// left                 right
+// 22                => 22
