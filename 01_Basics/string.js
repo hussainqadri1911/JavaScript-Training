8e85edaf561df605da65085f574593d8123bffc4
hussainqadri1911 <yourownwebdeveloper@gmail.com>
2024-10-20 05:50:49 +0000

    string file completed

---
+++ 01_Basics/string.js
@@ -0,0 +1,17 @@
+const myName = "Muhammad Hussain"
+const myAge = 22
+
+console.log(myName + myAge) //Avoid
+
+console.log(`My name is ${myName} and I am ${myAge} years old.`) // Modern syntax
+
+// Strings Methods:
+console.log(myName.charAt(2));
+console.log(myName.indexOf('s'));
+console.log(myName.substring(9, 15));
+console.log(myName.slice(-9, 13));
+console.log(myName.replace("Hussain", "Hasan"));
+console.log(myName.includes('Hussain'));
+console.log(myName.includes('Hasan'));
+console.log(myName.split(' '));
+
